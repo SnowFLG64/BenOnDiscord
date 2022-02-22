a89a66e1cba75bb16f5367802fe10a80576f8784
Sparrkz <71980947+sparrkzz@users.noreply.github.com>
2022-02-21 20:06:11 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,48 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const fs = require('fs');
 const config = JSON.parse(fs.readFileSync('./config.json'))
-
-// check config.json
 client.login(config.token);
 
-client.on('ready', () => console.log('Ben On Discord'));
+const randomactions = [
+  "Eat",
+  "Drink",
+  "Newspaper"
+]
+
+client.on('ready', () => { 
+  console.log('Ben On Discord')
+  var serverconfig = JSON.parse(fs.readFileSync('./server_config.json'));
+  var newspaperstate = 'closed'
+  action = randomactions[Math.floor(Math.random() * randomactions.length)]
+  if (action == "Newspaper") {
+    if (newspaperstate == 'closed') {
+      action = 'OpenNewspaper';
+      newspaperstate = 'open';
+    } else {
+      action = 'CloseNewspaper';
+      newspaperstate = 'closed';
+    }
+  }
+  serverconfig.forEach((srv) => {
+    console.log(srv.benchannel)
+    benChannel = client.channels.fetch(srv.benchannel).then(channel => channel.send(`https://sparrkz.tk/dumb-files/${action}.mp4`))
+  })
+  setInterval (function () {
+    action = randomactions[Math.floor(Math.random() * randomactions.length)]
+    if (action == "Newspaper") {
+      if (newspaperstate == 'closed') {
+        action = 'OpenNewspaper';
+        newspaperstate = 'open';
+      } else {
+        action = 'CloseNewspaper';
+        newspaperstate = 'closed';
+      }
+    }
+    serverconfig.forEach((srv) => {
+      benChannel = client.channels.fetch(srv.benchannel).then(channel => channel.send(`https://sparrkz.tk/dumb-files/${action}.mp4`))
+    })
+  }, 3600000);
+});
 
 const responses = [
   "Ben",
@@ -20,12 +57,9 @@ const responses = [
 
 client.on('messageCreate', (message) => {
   if (message.author !== client.user) {
-    // get server settings
     const serverconfig = JSON.parse(fs.readFileSync('./server_config.json'));
     var n = serverconfig.findIndex(x => x.server == message.guild.id)
-
     if (message.content.startsWith('b!talk')) {
-    // talk to ben
       if (n == "-1") {
         message.channel.send('Ben On Discord Not Set Up (b!setup)')
       } else {
@@ -34,7 +68,6 @@ client.on('messageCreate', (message) => {
         var isBen = true;
       }
     } else if (message.content.startsWith('b!setup')) {
-    // set up ben
       if (n == "-1") {
         let entry = {
           "server": message.guild.id,
@@ -51,7 +84,6 @@ client.on('messageCreate', (message) => {
         return;
       }
     } else if (message.content.startsWith('b!videos')) {
-    // change ben's video setting
       if (n == "-1") {
         message.channel.send('Ben On Discord Not Set Up (b!setup)')
       } else {
@@ -79,7 +111,6 @@ client.on('messageCreate', (message) => {
         return;
       }
     } else if (message.content.startsWith('b!benchannel')) {
-    // set the talking ben channel
       if (n == "-1") {
         message.channel.send('Ben On Discord Not Set Up (b!setup)')
       } else {
@@ -106,24 +137,19 @@ client.on('messageCreate', (message) => {
         }
       }
     } else if ((message.content.startsWith('b!help')) || (message.mentions.has(client.user) && message.content.includes('help'))) {
-    // help command
       if (n == "-1" ) {
-        message.channel.send(`b!setup - Set Up Ben On Discord`);
+        message.channel.send(`\`b!setup\` - Set Up Ben On Discord`);
       } else {
-        message.channel.send(`b!talk <say anything> - Talk To Ben On Discord`);
-        message.channel.send(`b!videos - Change If Ben On Discord Sends Videos Or Not`);
-        message.channel.send(`b!benchannel <channel-id || 'unset'> - Set A Channel Where Ben On Discord Responds Messages And Sends Things Randomly`);
+        message.channel.send(`\`b!talk <say anything>\` - Talk To Ben On Discord`);
+        message.channel.send(`\`b!videos\` - Change If Ben On Discord Sends Videos Or Not`);
+        message.channel.send(`\`b!benchannel\` <channel-id || 'unset'> - Set A Channel Where Ben On Discord Responds Messages And Sends Things Randomly`);
       }
       return;
     } else if (n !== "-1" && serverconfig[n]["benchannel"] == message.channel.id) {
-    // respond inside of talking ben channel
       var saying = message.content;
       var isBen = true;
     }
     if (isBen) {
-    // responses to b!talk
-
-      // set responses (based off of IShowSpeed's interactions)
       var response = responses[Math.floor(Math.random() * responses.length)];
       if (saying.toLowerCase().includes('are you gay')) {
         response = 'Yes';
@@ -140,18 +166,11 @@ client.on('messageCreate', (message) => {
       } else if (saying.toLowerCase().includes('stop')) {
         response = 'No';
       }
-
-      // mock user
       if (response == "USERMESSAGE") {
         if (saying == "") {
           var newresponses = responses.splice(0, responses.length - 1);
-          var response = newresponses[Math.floor(Math.random() * newresponses.length)];
+          var response = 'No'
           if (serverconfig[n]["videos"] == "true") {
-            if (response == "Ho Ho Ho") {
-              response = "Laugh";
-            } else if (response == "Na Na Na Na") {
-              response = "NaNaNaNa";
-            }
             message.channel.send(`https://sparrkz.tk/dumb-files/${response}.mp4`)
           } else {
             message.channel.send(response);
@@ -159,19 +178,31 @@ client.on('messageCreate', (message) => {
         }
         message.channel.send(`**${saying}**`);
       } else {
-      // send video response
         if (serverconfig[n]["videos"] == "true") {
           if (response == "Ho Ho Ho") {
             response = "Laugh";
-          } else if (response == "Na Na Na Na") {
+          }
+          if (response == "Na Na Na Na") {
             response = "NaNaNaNa";
           }
           message.channel.send(`https://sparrkz.tk/dumb-files/${response}.mp4`)
         } else {
-        // send text response
           message.channel.send(response);
         }
       }
+      console.log(`${message.member.user.tag} (${message.author.id}) in ${message.guild.name} (${message.guild.id}): ${message.content} ||||| Ben: ${response}`)
+      const log = JSON.parse(fs.readFileSync('./log.json'));
+      var entry = {
+        "authortag": message.member.user.tag,
+        "authorid": message.member.id,
+        "servername": message.guild.name,
+        "serverid": message.guild.id,
+        "content": message.content,
+        "ben": response
+      }
+      log.unshift(entry)
+      var newJson = JSON.stringify(log, null, 2);
+      fs.writeFileSync(`./log.json`, newJson);
     }
   }
 });
